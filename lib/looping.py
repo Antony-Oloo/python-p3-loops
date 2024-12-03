c0ab8a23a18693984ec43d1895fdd6c1f60c3060
antony <antoloo254@gmail.com>
2024-12-03 21:47:49 +0300

    All tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
+# Function for Happy New Year countdown
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+# Function to square integers in a list
+def square_integers(numbers):
+    return [x ** 2 for x in numbers]
 
+# FizzBuzz function for numbers from 1 to 100
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
